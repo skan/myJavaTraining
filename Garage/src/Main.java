d2b71b6eea755ec4faee085d8e3ffd84b28f2431
hamzask <shamza@microfocus.com>
2018-10-12 14:25:03 +0100

    load garage implemented

--- Garage/src/Main.java
+++ Garage/src/Main.java
@@ -1,13 +1,22 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
+
 
 public class Main {
 	public static void main(String[] args) {
 		Garage garage = new Garage();
 		System.out.println(garage);
-		save(garage);
+		//save(garage);
+		garage = load ();
+		System.out.println(garage);
 	}
-	
+
 	static void save(Garage garage) {
 		try {
 			String filepath = "garage.txt";
@@ -15,9 +24,39 @@ public class Main {
 			ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
 			objectOut.writeObject(garage);
 			objectOut.close();
+			fileOut.close();
 			System.out.println("The Garage is succesfully saved");
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
+
+	static Garage load() {
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		Garage garage = new Garage();
+
+		try {
+			fis = new FileInputStream(new File("garage.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			ois = new ObjectInputStream(fis);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			garage = (Garage) ois.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return garage;
+	}
+
 }

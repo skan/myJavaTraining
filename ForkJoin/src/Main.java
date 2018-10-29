a80d7b9ac499e6b5048d216d95d560cdaafd36d6
hamzask <shamza@microfocus.com>
2018-10-29 14:35:16 +0100

    join/fork implementation

--- ForkJoin/src/Main.java
+++ ForkJoin/src/Main.java
@@ -1,21 +1,27 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Path chemin = Paths.get("C:\\Users\\hamzask");
 		String filtre = "*.psd";
+
+		// Création de notre tâche principale qui se charge de découper son travail en
+		// sous-tâches
 		FolderScanner fs = new FolderScanner(chemin, filtre);
 
-		try {
-			Long start = System.currentTimeMillis();
-			Long resultat = fs.sequentialScan();
-			Long end = System.currentTimeMillis();
-			System.out.println("Il y a " + resultat + " fichier(s) portant l'extension " + filtre);
-			System.out.println("Temps de traitement : " + (end - start));
-		} catch (ScanException e) {
-			e.printStackTrace();
-		}
+		// Nous récupérons le nombre de processeurs disponibles
+		int processeurs = Runtime.getRuntime().availableProcessors();
+		// Nous créons notre pool de thread pour nos tâches de fond
+		ForkJoinPool pool = new ForkJoinPool(processeurs);
+		Long start = System.currentTimeMillis();
+
+		// Nous lançons le traitement de notre tâche principale via le pool
+		pool.invoke(fs);
+
+		Long end = System.currentTimeMillis();
+		System.out.println("Il y a " + fs.getResultat() + " fichier(s) portant l'extension " + filtre);
+		System.out.println("Temps de traitement : " + (end - start));
 	}
 }
